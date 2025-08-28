9db36f62b4bb063143f51e23ddd12d33defa5f8a
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-08-28 12:08:00 +0100

    Config update

--- app/weather_chat/services.py
+++ app/weather_chat/services.py
@@ -1,10 +1,10 @@
 from logging import getLogger
 
-import boto3
 from mcp import ClientSession
 from mcp.client.streamable_http import streamablehttp_client
 
 from app.config import config
+from app.bedrock.client import client
 from app.weather_chat.agents import WeatherAgent
 
 logger = getLogger(__name__)
@@ -13,11 +13,6 @@ logger = getLogger(__name__)
 async def process_weather_message(message: str):
     async with streamablehttp_client(config.mcp_url) as (r, w, _), ClientSession(r, w) as mcp_session:
         await mcp_session.initialize()
-
-        client = boto3.client(
-            "bedrock-runtime",
-            region_name=config.aws_region
-        )
 
         weather_agent = WeatherAgent(client, mcp_session)
 
